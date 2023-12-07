edea16841ee38f8bf9c2097fd55fe8ec2a6deed6
Josh <jks.hardy@gmail.com>
2023-12-07 12:53:47 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div class="min-h-screen w-full flex flex-col items-center justify-center p-6 z-50 gap-y-12">
 	<div class="flex flex-col items-center gap-y-2">
 		<img class="w-24" src={logo} />
-		<span class="font-bold text-3xl">Rain Protocol</span>
+		<span class="font-bold text-3xl">Rain Language</span>
 	</div>
 	<p class="max-w-lg text-center">
 		Rain exists to make smart contracts accessible to <span class="text-[#923c38]">everyone</span>,
@@ -18,15 +18,15 @@
 	</p>
 	<div>
 		<a class="link" href="https://github.com/rainlanguage">Github</a>
-		<a class="link" href="https://docs.rainprotocol.xyz/core-concepts/introduction">Core concepts</a
+		<a class="link" href="https://docs.rainlang.xyz/core-concepts/introduction">Core concepts</a
 		>
-		<a class="link" href="https://docs.rainprotocol.xyz/orderbook/introduction">Orderbook</a>
+		<a class="link" href="https://docs.rainlang.xyz/orderbook/introduction">Orderbook</a>
 		<a
 			class="link"
 			href="https://docs.rainprotocol.xyz/learning-rainlang/introduction/introduction-to-rainlang"
 			>Learning Rainlang</a
 		>
-		<a class="link" href="https://docs.rainprotocol.xyz/blog">Blog</a>
+		<a class="link" href="https://docs.rainlang.xyz/blog">Blog</a>
 	</div>
 </div>
 
