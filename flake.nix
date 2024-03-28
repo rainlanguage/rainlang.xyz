e24563af4e9385927387cb135b1758d2d835b6b0
thedavidmeister <thedavidmeister@gmail.com>
2024-03-28 16:58:30 +0400

    fmt

--- flake.nix
+++ flake.nix
@@ -10,12 +10,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       rec {
         packages = rainix.packages.${system} // {
-            static-check = rainix.pkgs.${system}.writeShellScriptBin "static-check" ''
-                prettier --check .
-            '';
+            static-check = rainix.mkTask.${system} {
+                name = "static-check";
+                body = ''
+                    set -euxo pipefail
+                    npm exec prettier -- --check .
+                '';
+            };
+
+            prettier-fmt = rainix.mkTask.${system} {
+                name = "prettier-fmt";
+                body = ''
+                    set -euxo pipefail
+                    npm exec prettier -- --write .
+                '';
+            };
         };
-        devShells.default = rainix.devShells.${system}.default // {
-            packages = [packages.static-check];
+
+        devShells.default = rainix.pkgs.${system}.mkShell {
+          packages = [
+            packages.static-check
+            packages.prettier-fmt
+          ];
+          shellHook = ''
+            npm install --verbose
+          '';
+          buildInputs = rainix.devShells.${system}.default.buildInputs ++ [];
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
         };
       }
     );
