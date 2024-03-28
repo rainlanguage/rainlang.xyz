8014e7276331300850923bece24a6c2a03823e68
thedavidmeister <thedavidmeister@gmail.com>
2024-03-28 16:36:54 +0400

    wip on flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,15 @@
 
   outputs = {self, flake-utils, rainix }:
     flake-utils.lib.eachDefaultSystem (system:
-      {
-        packages = rainix.packages.${system};
-        devShells = rainix.devShells.${system};
+      rec {
+        packages = rainix.packages.${system} // {
+            static-check = rainix.pkgs.${system}.writeShellScriptBin "static-check" ''
+                prettier --check .
+            '';
+        };
+        devShells.default = rainix.devShells.${system}.default // {
+            packages = [packages.static-check];
+        };
       }
     );
 
