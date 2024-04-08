3f89a6182473823ea7942fef14164c91c557fd6c
highonhopium <jks.hardy@gmail.com>
2024-04-08 22:27:15 +0100

    adding raindex to site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,42 @@
 <script>
 	import '../app.postcss';
 	import logo from '$lib/logo.svg';
+	import raindexlogo from '$lib/raindex-logo.svg';
 </script>
 
 <div class="min-h-screen w-full flex flex-col items-center justify-center p-6 z-50 gap-y-12">
 	<div class="flex flex-col items-center gap-y-2">
+		<img class="w-96" src={raindexlogo} />
+	</div>
+	<p class="max-w-lg text-center">
+		Raindex allows anyone to write, deploy and manage token trading strategies, written in Rainlang,
+		on any EVM network.
+	</p>
+	<div class="flex justify-center flex-wrap gap-2">
+		<a
+			class="link bg-blue-700 border-none text-white"
+			href="https://docs.rainlang.xyz/raindex/download">Download app</a
+		>
+		<a class="link" href="https://docs.rainlang.xyz/raindex/example-strats/writing-strategies"
+			>Learn Raindex</a
+		>
+	</div>
+	<div
+		class="h-[1px] bg-gradient-to-r from-transparent via-blue-500 to-transparent max-w-lg w-full my-4"
+	/>
+	<div class="flex flex-col items-center gap-y-2">
 		<img class="w-96" src={logo} />
 	</div>
 	<p class="max-w-lg text-center">
-		Rainlang is a smart contract language designed to be easier to read and write. If you can write
-		an Excel formula, you can learn to write Rainlang.
+		Rainlang is defi's native language. If you know your way around a spreadsheet, you can learn
+		Rain.
 	</p>
 	<div class="flex justify-center flex-wrap gap-2">
 		<a class="link" href="https://github.com/rainlanguage">Github</a>
 		<a class="link" href="https://docs.rainlang.xyz/core-concepts/introduction">Core concepts</a>
-		<a class="link" href="https://docs.rainlang.xyz/orderbook/introduction">Raindex</a>
 		<a
 			class="link"
-			href="https://docs.rainprotocol.xyz/learning-rainlang/introduction/introduction-to-rainlang"
+			href="https://docs.rainlang.xyz/learning-rainlang/introduction/introduction-to-rainlang"
 			>Learning Rainlang</a
 		>
 		<a class="link" href="https://docs.rainlang.xyz/blog">Blog</a>
